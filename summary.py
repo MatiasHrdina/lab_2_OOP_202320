8a59cc787b59f3a41fb5c94cecf16d6a54c8e3a3
MatiasHrdina <mshrdina@miuandes.cl>
2023-08-20 17:44:57 -0400

    commit 9

--- summary.py
+++ summary.py
@@ -7,14 +7,19 @@
 class Summary:
     def __init__(self, Dictionary):
         self._data = data
-        self._frequencies = frequencies
+        self._frequencies = []
 
     def call(self):
         self._build_summary()
         
     def _build_summary(self):
-        for element in data:
+        for element in self._data:
             self._process_element(element)
 
-    def _process_element(self, ):
-        
+    def _process_element(self, element):
+        processed = False
+        for registered in self._frequencies:
+          
+            
+        if not processed :
+            self._frequencies.append({'local_nombre' : element['local_nombre'],'value' : 1})
