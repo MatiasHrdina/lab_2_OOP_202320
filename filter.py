b92f5ae83cd7be390109b07562702adf79ca970d
MatiasHrdina <mshrdina@miuandes.cl>
2023-08-20 16:50:20 -0400

    commit 6

--- filter.py
+++ filter.py
@@ -3,3 +3,4 @@
 #apply(): Float
 
 class Filter:
+    
--- summary.py
+++ summary.py
@@ -6,4 +6,12 @@
 #process_element(): None
 
 class Summary:
-    
+    def __init__(self, Dictionary):
+        self._data = data
+        self._frequencies = frequencies
+
+    def call():
+
+    def _build_summary():
+
+    def _process_element():
