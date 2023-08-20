05b9011395c35df4cb2f3a72ec64c8f8a5421e5b
MatiasHrdina <mshrdina@miuandes.cl>
2023-08-20 17:27:13 -0400

    commit 8

--- summary.py
+++ summary.py
@@ -4,15 +4,17 @@
 #call(): Dictionary
 #build_summary(): None
 #process_element(): None
-
 class Summary:
     def __init__(self, Dictionary):
         self._data = data
         self._frequencies = frequencies
 
     def call(self):
-        pass
+        self._build_summary()
+        
     def _build_summary(self):
-        pass
+        for element in data:
+            self._process_element(element)
+
     def _process_element(self, ):
-        pass
+        
